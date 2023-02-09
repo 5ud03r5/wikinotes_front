8d0a5c74d0d019f940e4e71bcb0516ceb3c1f13d
5ud03r5 <94323029+5ud03r5@users.noreply.github.com>
2023-02-09 15:05:18 +0100

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,6 @@ const Logo = () => {
   return (
     <NavbarBrand className={classes.navlogo} href="/">
       <span className={classes.wikinotes}>WIKI NOTES</span>
-      {/* <img alt="logo" src="/atos1.png" style={{ heigth: 25, width: 80 }} /> */}
     </NavbarBrand>
   );
 };
